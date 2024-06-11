a843c30f6c981da2c82cff8a6753b4c6ae097091
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-06-11 16:27:14 +0100

    new API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '0a49d29d-cbab-4226-9b8c-651fa8215d07';
+export const KEY = '16213865-5fcb-4e12-b452-47ccf53a4a41';
 export const MODAL_CLOSE_SEC = 2.5;
